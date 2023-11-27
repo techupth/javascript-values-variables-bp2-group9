7cfa9605b06b777e4671b1e670747cf2a83b7884
KRITTIRIN <temzaaa4@gmail.com>
2023-11-27 20:57:48 +0700

    feat(Valuse-variable) : commplete exercse

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,15 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstName = "Karter";
+let lastName = "Anderson";
+let userAddress = "54/24 Gp5 Bangkruay-Sainoui Kruay Bang Kruai";
+const resTaurant = "Chef's Table by Chef James";
+const totalFoodCost = 15000;
+const openingResTaurant = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+userAddress =  "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+totalFoodCost = 5000
+openingResTaurant = 2500 ;
+
