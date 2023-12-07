b1a8b2b7de27e268639ad4cc904a09813aa2771b
Worawat <kimcoop2561@gmail.com>
2023-12-08 01:08:49 +0700

    feat(excercise): Completed Values Variables

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,28 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let name = "Karter"; 
+console.log(name); 
+let surname = "Anderson"; 
+console.log(surname); 
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"; 
+console.log(address); 
+
+const nameResterant = "Chef's Table by Chef James"; 
+console.log(nameResterant); 
+const billFood = 15000; 
+console.log(billFood); 
+const amountResterantArea = 1500; 
+console.log(amountResterantArea);
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+let addressHouse = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"; 
+addressHouse = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"; 
+console.log(addressHouse); 
+let bill = 15000; 
+bill = 5000; 
+console.log(bill); 
+let amountResterant = 1500; 
+amountResterant = 2500; 
+console.log(amountResterant); 
+
